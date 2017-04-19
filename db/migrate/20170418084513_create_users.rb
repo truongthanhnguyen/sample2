52eab511d4ad670184826927d7d0992449121474
truongthanh <truongthanh.bk56@gmail.com>
2017-04-19 09:29:12 +0700

    finish user sign up

--- db/migrate/20170418084513_create_users.rb
+++ db/migrate/20170418084513_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    add_index :users, :email, unique: true
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.timestamps null: false
     end
   end
 end
